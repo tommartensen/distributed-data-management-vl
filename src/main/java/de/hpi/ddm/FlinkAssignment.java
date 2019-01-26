755cc81abedafbd592177a5293bb4d515fba682d
tommartensen <mail@tommartensen.de>
2019-01-26 16:26:25 +0100

    add avg response size per hour for successful requests

--- src/main/java/de/hpi/ddm/FlinkAssignment.java
+++ src/main/java/de/hpi/ddm/FlinkAssignment.java
@@ -11,8 +11,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.Collector;
 
 
@@ -33,15 +35,17 @@ public class FlinkAssignment {
 
 		SourceFunction<LogEntry> logEntrySource = new LogEntrySource(logPath);
 
-		DataStream<LogEntry> logEntries = env.addSource(logEntrySource).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<LogEntry>(Time.seconds(60)) {
 
-            @Override
-            public long extractTimestamp(LogEntry logEntry) {
-                return logEntry.timestamp.getMillis();
-            }
-        });
+		BoundedOutOfOrdernessTimestampExtractor<LogEntry> timestampExtractor = new BoundedOutOfOrdernessTimestampExtractor<LogEntry>(Time.seconds(60)) {
+			@Override
+			public long extractTimestamp(LogEntry logEntry) {
+				return logEntry.timestamp.getMillis();
+			}
+		};
+		DataStream<LogEntry> logEntries = env.addSource(logEntrySource).assignTimestampsAndWatermarks(timestampExtractor);
 
 		// Client that requested the most byte resources per day
+
 		DataStream<Tuple3<Long, String, Long>> bytesPerMonthAndClient = logEntries
 				.keyBy(e -> e.client)
 				.timeWindow(Time.days(1))
@@ -51,10 +55,10 @@ public class FlinkAssignment {
                 .timeWindowAll(Time.days(1))
                 .maxBy(2);
 
-        maxBytesPerDayAndClient.print("Client that requested the most byte resources per day (days in ms, client, transferred bytes) : ").setParallelism(1);
+        //maxBytesPerDayAndClient.print("Client that requested the most byte resources per day (days in ms, client, transferred bytes) : ").setParallelism(1);
 
         // Most requested resource (total count)
-        SingleOutputStreamOperator<Tuple2<String, Integer>> mostRequestedResource = logEntries
+		SingleOutputStreamOperator<Tuple2<String, Integer>> mostRequestedResource = logEntries
                 .flatMap(new ResourceMapper())
                 .keyBy(0)
                 .timeWindow(Time.hours(10))
@@ -64,8 +68,34 @@ public class FlinkAssignment {
 
         mostRequestedResource.print("Most requested resource within 10 hours (resource, accesses)").setParallelism(1);
 
+
+		SingleOutputStreamOperator<Tuple3<Long, String, Float>> avg = logEntries
+				.filter(e -> e.httpStatus == 200)
+				.filter(e -> e.httpMethod.equals("GET"))
+				.keyBy(e -> e.httpMethod)
+				.timeWindow(Time.hours(1))
+				.process(new MyProcessWindowFunction());
+
+				avg.print("Average number of transferred bytes per hour for GET requests with 200 : ").setParallelism(1);
+
         // execute program
 		env.execute("Flink Assignment");
+	}
+
+	public static class MyProcessWindowFunction extends ProcessWindowFunction<LogEntry, Tuple3<Long, String, Float>, String, TimeWindow> {
+
+		@Override
+		public void process(String key, Context context, Iterable<LogEntry> logEntries, Collector<Tuple3<Long, String, Float>> out) {
+			float sum = 0f;
+			float count = 0f;
+
+			for (LogEntry e : logEntries) {
+				sum += e.bytesTransferred;
+				count++;
+			}
+
+			out.collect(new Tuple3<>(context.window().getEnd(), key, (sum / count)));
+		}
 	}
 
 	public static class AddBytes extends ProcessWindowFunction<LogEntry, Tuple3<Long, String, Long>, String, TimeWindow> {
@@ -87,4 +117,5 @@ public class FlinkAssignment {
             out.collect(new Tuple2<>(value.resource, 1));
         }
     }
+
 }
